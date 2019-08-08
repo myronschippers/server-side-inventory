7ee12eda0d1a60a1683761ebf219f147f9b42cf3
Myron <dubtwee@gmail.com>
2019-08-08 14:02:25 -0500

    flesh out basic endpoints for getting inventory, searching inventory, and adding to the inventory

---
+++ server/modules/inventory.js
@@ -0,0 +1,8 @@
+const inventory = [
+    {
+        name: 'Dragon Egg',
+        description: 'Dormant egg for the European Ridgeback Dragon.'
+    }
+];
+
+module.exports = inventory;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = '5000';
+const inventory = require('./modules/inventory.js');
 
 // Kick off server application
 app.listen(port, () => {
@@ -14,3 +15,25 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes will go below here
+app.get('/api/inventory', (req, res) => {
+    res.send(inventory);
+});
+
+app.post('/api/inventory', (req, res) => {
+    const newInventoryItem = req.body;
+    inventory.push(newInventoryItem);
+
+    res.status(201);
+});
+
+app.post('/api/inventory/search', (req, res) => {
+    const searchResults = [];
+    const searchString = req.body;
+    for (let inventoryItem of inventory) {
+        if (inventoryItem.name.includes(searchString)) {
+            searchResults.push(inventoryItem);
+        }
+    }
+
+    res.send(searchResults);
+});
